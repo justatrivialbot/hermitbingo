30f5db89a0a00d0a6bbcaabdf4ce0256efba39d3
JustATrivialBot <justatrivialbot@gmail.com>
2022-03-04 21:06:45 -0600

    Update for S9

--- bingo.js
+++ bingo.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	
+
 	var newpath;
 	$('#faceselect').change(function() {
 		var newface = $('#faceselect option:selected').val();
@@ -7,43 +7,43 @@ $(document).ready(function(){
 		console.log(newpath);
 		$('#dabsample').css('background-image', newpath);
 	})
-	
+
 	var col1 = [
 		'"Small Little"',
 		'"Spoiler"/"Exclusive Preview"',
 		'Sl1pg8r lingo',
 		'[Hermit] went to bed. Sweet Dreams.',
 		'In-Game Chat',
-		'World Backup script',
 		'Mimicking Villager Honks',
-		'Namecheck HC Recap',
+		'Namecheck HC Recap/Elybeatmaker',
 		'"Derp" or "Spoon"',
 		'Namecheck a Mojang Employee',
-		'"Team STAR" or "G-Team"',
-		'Cleo\'s Ship',
-		'TAG building',
-		'Mumbo\'s Sphere',
-		'TFC\'s Sphere',
+        'Someone Else\'s Base',
+        'Inside of an Ender Chest',
+        'Getting stuck in a door',
+        'Build palette reference',
+        '"Decked Out"',
+        'Riding a mob',
 	];
-	
+
 	var col2 = [
 		"n1tpiCkY c0mm3nt",
 		"Commenter shoutout",
 		"Patreon Plug",
 		"Merch Plug",
-		"Twitch/Mixer/2nd Channel Plug",
+		"Twitch/2nd Channel Plug",
 		"\"Comment / Like / Subscribe / Bell\"",
 		"Fortune Pick Guessing Game",
 		"\"FIRST\" Comment",
 		"Creative World",
-		"Poll",
 		"Twitter Plug",
-		"False's Aqueduct",
-		"Concorp HQ",
-		"Scar's Landscaping Service",
-		"Grian's Coffeepot"
+        "Sponsored content",
+        "Weird Docm contraption",
+        "Moving Mobs",
+        'Wandering trader',
+        'Collab',
 	];
-	
+
 	var col3 = [
 		"Time Lapse",
 		"Accidental Asymmetry",
@@ -57,13 +57,12 @@ $(document).ready(function(){
 		"Over Half Empty Hunger Bar",
 		"Found diamonds!",
 		"Side torch",
-		"iBay",
-		"Iskall's Base",
-		"Ministry of Truth"
+        "Bed missing or obstructed",
+        "Chest monster",
+        "The Moon",
 	];
-	
+
 	var col4 = [
-		"Bumbo Pareidolia",
 		"Flying Background Hermit",
 		"Cam Account Login/Logout",
 		"Wither Bwongg / Dragon Death Crackle",
@@ -72,35 +71,36 @@ $(document).ready(function(){
 		"React to Another Hermit's Build",
 		"More than 3 chickens in frame",
 		"Llama!",
-		"Leftover Grian Head",
 		"Leaping Dolphin",
 		"Named Mob",
-		"Conduit buff",
-		"Stress's Chicken Land",
-		"Turtle inna boat"
+		"Mob in a boat",
+        "Totem of Undying",
+        "Dreading a nether trip",
+        "Running out of bubbles",
+        "Shop with a punny name",
 	];
-	
+
 	col5 = [
 		"Java.io.exception",
 		"Debug screen",
 		"\"[Any MC version number]\"",
-		"Checking Leaderboard",
 		"Bedrock or Build Limit",
 		"Protracted Redstoning",
-		"Namecheck Ilmango, Gnembon, JL2579 or Panda",
-		"Mob Farm (Titan counts)",
+		"Namecheck Hivemind/Scicraft/Contraption maker",
+		"Mob Farm",
 		"\"Optifine\"",
 		"\"Lag!\" /  \"#blameMumbo\"",
-		"The Stock Exchange",
-		"NHO Nature Reserve",
-		"Jevin's Castle",
-		"WebCo / WelsCo",
-		"Any Zedaph Base"
+        "Somebody Roleplaying",
+        "Game version confusion",
+        "Phantoms",
+        "Running out of rockets",
+        "Guardian Temple",
+        "Build without a back or interior",
 	]
-	
+
 	function shufflePop(arr,i,skipmiddle) {
 		var shuffled = arr.sort(() => .5 - Math.random());
-		
+
 		if (skipmiddle) {
 			var selected = shuffled.slice(0,4);
 			var selectorNums = [
@@ -122,14 +122,14 @@ $(document).ready(function(){
 		for (var i = 0; i < selectorNums.length; i++) {
 			$(selectorNums[i]).text(selected[i]);
 		}
-		
+
 	}
 	shufflePop(col1,1);
 	shufflePop(col2,2);
 	shufflePop(col3,3,true);
 	shufflePop(col4,4);
 	shufflePop(col5,5);
-	
+
 	$('.cell').click(function() {
 		if ($(this).css('background-image') != 'none') {
 			$(this).css({
